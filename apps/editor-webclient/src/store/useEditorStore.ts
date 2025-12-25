62087e7a9ea8015b345c284de6464525d314c617
Feroz Ahmmed <froghramar@gmail.com>
2025-12-25 15:38:52 +0600

    fix(useEditorStore): add BPMN file template when no content is provided

--- apps/editor-webclient/src/store/useEditorStore.ts
+++ apps/editor-webclient/src/store/useEditorStore.ts
@@ -464,7 +464,30 @@ export const useEditorStore = create<EditorState>((set, get) => ({
   createFile: async (path, type, content) => {
     set({ isLoading: true, error: null });
     try {
-      await fileApi.createFile(path, type, content);
+      // If creating a BPMN file and no content provided, use template
+      let fileContent = content;
+      if (type === 'file' && path.toLowerCase().endsWith('.bpmn') && !content) {
+        fileContent = `<?xml version="1.0" encoding="UTF-8"?>
+<bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" 
+                   xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" 
+                   xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" 
+                   xmlns:di="http://www.omg.org/spec/DD/20100524/DI" 
+                   id="Definitions_1" 
+                   targetNamespace="http://bpmn.io/schema/bpmn">
+  <bpmn:process id="Process_1" isExecutable="false">
+    <bpmn:startEvent id="StartEvent_1" />
+  </bpmn:process>
+  <bpmndi:BPMNDiagram id="BPMNDiagram_1">
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1">
+      <bpmndi:BPMNShape id="_BPMNShape_StartEvent_1" bpmnElement="StartEvent_1">
+        <dc:Bounds x="173" y="102" width="36" height="36" />
+      </bpmndi:BPMNShape>
+    </bpmndi:BPMNPlane>
+  </bpmndi:BPMNDiagram>
+</bpmn:definitions>`;
+      }
+      
+      await fileApi.createFile(path, type, fileContent);
       
       // Reload the parent folder to show the new file
       const parentPath = dirname(path);
